a4c1aa5e1c4b9639b0afaf0217c621c9b250e83a
Patrick Richardson Wright <wrightmight@gmail.com>
2022-07-16 11:48:31 -0700

    change all html to hbs

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 const sequelize = require('./config/connection');
-
+const handlebars = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+app.set('view engine', 'hbs');
+
+app.engine(
+  'hbs',
+  handlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials/',
+    extname: 'hbs',
+  })
+);
+
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () =>
+    console.log(`Now listening at http://localhost:${PORT}`)
+  );
 });
