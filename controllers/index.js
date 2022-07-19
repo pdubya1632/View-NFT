5665593c51453d00c2e80245364ddf47aae8d845
Samuel Scroggs <102083372+sscroggs11@users.noreply.github.com>
2022-07-19 16:16:47 -0700

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const homeRoutes = require("./home-routes");
 const commentRoutes = require("./api/comment-routes");
 const userRoutes = require("./api/user-routes");
-const postRoutes = require("./api/post-routes");
+const postRoutes = require("./api/gallery-routes");
+const nftRoutes = require("./api/nft-routes");
 //set up the route useage
 router.use("/", homeRoutes);
 router.use("/api/comments", commentRoutes);
 router.use("/api/users", userRoutes);
-router.use("/api/posts", postRoutes);
+router.use("/api/posts", galleryRoutes);
+router.use("/api/nfts", nftRoutes);
 module.exports = router;
