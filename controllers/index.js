9333a7a5689bad7cbd33691f1db7b6cce1e6aa0b
Samuel Scroggs <102083372+sscroggs11@users.noreply.github.com>
2022-07-19 15:21:06 -0700

    Update index.js
    nft routes is likely where the function to send the alchemy json response to the database

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
-
+const router = require("express").Router();
+const homeRoutes = require("./home-routes");
+const commentRoutes = require("./api/comment-routes");
+const userRoutes = require("./api/user-routes");
+const postRoutes = require("./api/post-routes");
+//set up the route useage
+router.use("/", homeRoutes);
+router.use("/api/comments", commentRoutes);
+router.use("/api/users", userRoutes);
+router.use("/api/posts", postRoutes);
 module.exports = router;
