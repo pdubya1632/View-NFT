57cf7dd599b2ce00f2093de563bdde7a1ee7be25
Patrick Richardson Wright <wrightmight@gmail.com>
2022-07-18 21:46:33 -0700

    revert back to orig alchemy.js

--- alchemy.js
+++ alchemy.js
@@ -1,27 +1,37 @@
 import {
-    getNftsForOwner,
-    getNftsForOwnerIterator,
-    NftExcludeFilters,
-    initializeAlchemy,
-  } from "@alch/alchemy-sdk";
-  
-  const alchemy = initializeAlchemy();
-  
-  // Get how many NFTs an address owns.
-  getNftsForOwner(alchemy, "0xshah.eth").then((nfts) => {
-    console.log(nfts.totalCount);
-  });
-  
-  // Get all the image urls for all the NFTs an address owns.
-  async function main() {
-    for await (const nft of getNftsForOwnerIterator(alchemy, "0xshah.eth")) {
-      console.log(nft.rawMetadata,nft.media);
-    }
+  getNftsForOwner,
+  getNftsForOwnerIterator,
+  NftExcludeFilters,
+  initializeAlchemy,
+} from '@alch/alchemy-sdk';
+
+const alchemy = initializeAlchemy();
+
+// Get how many NFTs an address owns.
+getNftsForOwner(
+  alchemy,
+  '0x9Ad80ABc02bA459d7A7E252b3d173F471Daa1012'
+).then((nfts) => {
+  console.log(nfts.totalCount);
+});
+
+// Get all the image urls for all the NFTs an address owns.
+async function main() {
+  for await (const nft of getNftsForOwnerIterator(
+    alchemy,
+    '0x9Ad80ABc02bA459d7A7E252b3d173F471Daa1012'
+  )) {
+    console.log(nft.rawMetadata, nft.media);
   }
-  
-  main();
-  
-  // Filter out spam NFTs.
-  getNftsForOwner(alchemy, "0xshah.eth", {
+}
+
+main();
+
+// Filter out spam NFTs.
+getNftsForOwner(
+  alchemy,
+  '0x9Ad80ABc02bA459d7A7E252b3d173F471Daa1012',
+  {
     excludeFilters: [NftExcludeFilters.SPAM],
-  }).then(console.log);
+  }
+).then(console.log);
