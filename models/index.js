f5730dd13ed75ec1666860d1a30c90bff8976a2e
Samuel Scroggs <102083372+sscroggs11@users.noreply.github.com>
2022-07-19 21:09:57 -0700

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,39 @@
+// This is where we will set up relationships for the models
+// Import all the models
+const User = require("./User");
+const Gallery= require("./Gallery");
+const Comment = require("./Comment");
+const Nft = require("./Nft")
+
+//set up relationships
+User.hasMany(Gallery,{as: 'galleries',
+  foreignKey: "user_id"
+})
+Gallery.belongsTo(User, {
+  foreignKey: "user_id"
+});
+Gallery.hasMany(Nft, {
+  as: "nfts",
+  foreignKey: "nft_id",
+});
+Nft.belongsTo(Gallery, {
+  foreignKey: "nft_id",
+});
+//associations for the commments
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Gallery, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Gallery.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Gallery, Comment,Nft };
