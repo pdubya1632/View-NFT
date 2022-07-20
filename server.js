437a57c7945c6c77ad2e56f806b4845702f23d0c
Patrick Richardson Wright <wrightmight@gmail.com>
2022-07-19 22:03:31 -0700

    correct server.js file

--- server.js
+++ server.js
@@ -1,31 +1,46 @@
 const express = require('express');
-const routes = require('./routes');
+const path = require('path');
+//paths
+const controller = require('./controllers');
+//handlebars
+const exphbs = require('express-handlebars');
+//Sequelize
 const sequelize = require('./config/connection');
-const handlebars = require('express-handlebars');
+//Session
+const session = require('express-session');
+const SequlizeStore = require('connect-session-sequelize')(
+  session.Store
+);
 
+//set up the actual session
+const sess = {
+  secret: 'super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequlizeStore({
+    db: sequelize,
+  }),
+};
+//initialize the server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//middlewear
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
-app.use(routes);
+//use controllers
+app.use('/', controller);
 
-app.set('view engine', 'hbs');
+//set handlebars as render engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-app.engine(
-  'hbs',
-  handlebars.engine({
-    layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials/',
-    extname: 'hbs',
-  })
-);
-
-// sync sequelize models to the database, then turn on the server
-// sequelize.sync({ force: false }).then(() => {
-app.listen(PORT, () =>
-  console.log(`Now listening at http://localhost:${PORT}`)
-);
-// });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(`Now listening on port ${PORT}`)
+  );
+});
