1d37455a2d5ec3132084ec2fe0691d02a9676a59
Patrick Richardson Wright <wrightmight@gmail.com>
2022-07-21 17:02:07 -0700

    add data onto page

--- public/js/quicknode.js
+++ public/js/quicknode.js
@@ -13,6 +13,9 @@
 // module.exports = fetchNfts;
 
 const fetchNFTs = async (e) => {
+  const collectionGrid = document.getElementById('collectionGrid');
+  const addCollection = document.getElementById('addCollection');
+
   console.log('fetching nfts');
   const api_key = 'A8A1Oo_UTB9IN5oNHfAc2tAxdR4UVwfM';
   const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
@@ -38,19 +41,38 @@ const fetchNFTs = async (e) => {
     return container;
   });
 
-  if (filterNfts) {
-    const response = await fetch('/api/nfts', {
-      method: 'post',
-      body: JSON.stringify({
-        filterNfts,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  filterNfts.forEach((element) => {
+    collectionGrid.innerHTML += `<div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">`;
+    collectionGrid.innerHTML += `<article class="overflow-hidden rounded-lg shadow-lg">`;
+    collectionGrid.innerHTML += `<a href="#">`;
+    collectionGrid.innerHTML += `<img alt="Placeholder" class="block h-auto w-full" src="${element.image_url}" />`;
+    collectionGrid.innerHTML += `</a>`;
+    collectionGrid.innerHTML += `<header class="flex items-center justify-between leading-tight p-2 md:p-4">`;
+    collectionGrid.innerHTML += `<h2 class="text-lg">`;
+    collectionGrid.innerHTML += `<a class="no-underline hover:underline text-white" href="#">${element.title}</a>`;
+    collectionGrid.innerHTML += `</h2>`;
+    collectionGrid.innerHTML += `<p class="text-white text-sm">${element.description}</p>`;
+    collectionGrid.innerHTML += `</header>`;
+    collectionGrid.innerHTML += `<footer class="flex items-center justify-between leading-none p-2 md:p-4"></footer>`;
+    collectionGrid.innerHTML += `</article>`;
+    collectionGrid.innerHTML += `</div>`;
+  });
 
-    if (response.ok) {
-      console.log(response);
-    } else {
-      alert(response.statusText);
-    }
-  }
+  addCollection.classList.add('hidden');
+
+  // if (filterNfts) {
+  //   const response = await fetch('/api/nfts', {
+  //     method: 'post',
+  //     body: JSON.stringify({
+  //       filterNfts,
+  //     }),
+  //     headers: { 'Content-Type': 'application/json' },
+  //   });
+
+  //   if (response.ok) {
+  //     console.log(response);
+  //   } else {
+  //     alert(response.statusText);
+  //   }
+  // }
 };
