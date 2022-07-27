cbf6ed2842b5d63bc83f29fe16231053c8053598
Patrick Richardson Wright <wrightmight@gmail.com>
2022-07-26 20:04:06 -0700

    clean up home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { User, Nft, Comment } = require('../models');
-const sequelize = require('../config/connection');
+// const sequelize = require('../config/connection');
 
-/* home view */
+// home view
 router.get('/', (req, res) => {
   res.render('home-main', {
     layout: 'home',
@@ -10,29 +10,45 @@ router.get('/', (req, res) => {
   });
 });
 
-/* user registration view */
+// registration view
 router.get('/register', (req, res) => {
   viewPage = () => 'register';
   res.render('register-main', { layout: 'register' });
 });
 
-/* forgot password view */
-router.get('/password-reset', (req, res) => {
-  res.render('password-reset-main', { layout: 'password-reset' });
-});
-
-/* nft detail dashboard view */
-/* route should be changed to /dashboard/nft/:id */
-router.get('/dashboard/nft/', (req, res) => {
-  res.render('nftDetail-main', {
-    layout: 'dashboard',
+// login view
+router.get('/login', (req, res) => {
+  console.log('Is logged in?', req.session.loggedIn);
+  res.render('login-main', {
+    layout: 'login',
     loggedIn: req.session.loggedIn,
   });
 });
 
-//serve up the single gallery page
+// forgot password view
+router.get('/password-reset', (req, res) => {
+  res.render('password-reset-main', { layout: 'password-reset' });
+});
+
+// dashboard view
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard-main', {
+    layout: 'dashboard',
+    userId: req.session.user_id,
+  });
+});
+
+// nft detail dashboard view
+// route should be changed to /dashboard/nft/:id
+// router.get('/dashboard/nft/', (req, res) => {
+//   res.render('nftDetail-main', {
+//     layout: 'dashboard',
+//     loggedIn: req.session.loggedIn,
+//   });
+// });
+
+// show individual nft
 router.get('/viewnft/:id', (req, res) => {
-  //we need to get all galleries
   Nft.findOne({
     where: {
       id: req.params.id,
@@ -77,43 +93,6 @@ router.get('/viewnft/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-});
-
-//serve up the login page
-router.get('/login', (req, res) => {
-  console.log('Is logged in?', req.session.loggedIn);
-  res.render('login-main', {
-    layout: 'login',
-    loggedIn: req.session.loggedIn,
-  });
-});
-
-/* dashboard view */
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard-main', {
-    layout: 'dashboard',
-    userId: req.session.user_id,
-  });
-});
-
-router.get('/dashboard/collection/nft/:id', (req, res) => {
-  res.render('nftDetail-main', {
-    layout: 'dashboard',
-    userId: req.session.user_id,
-  });
-});
-
-// router.get('/gallery', (req, res) => {
-//   res.render('create-gallery', { loggedIn: req.session.loggedIn });
-// });
-
-//load the edit page
-router.get('/edit/:id', (req, res) => {
-  //    gallery_id: req.galleryID,
-  res.render('edit-gallery', {
-    loggedIn: req.session.loggedIn,
-    gallery_id: req.params.id,
-  });
 });
 
 module.exports = router;
